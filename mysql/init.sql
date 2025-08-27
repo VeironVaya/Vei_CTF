cff31ae40f7a53806f0ed861f0080c1f918abc9d
VeironVaya <veironvayayarief41@gmail.com>
2025-08-27 10:36:51 +0700

    v7
    add sql layer

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,15 +1,10 @@
-CREATE DATABASE IF NOT EXISTS ctf;
-USE ctf;
+CREATE DATABASE IF NOT EXISTS ctfdb;
 
-CREATE TABLE IF NOT EXISTS flags (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  flag VARCHAR(255) NOT NULL
+USE ctfdb;
+
+CREATE TABLE IF NOT EXISTS flag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    flag VARCHAR(255) NOT NULL
 );
-INSERT INTO flags (flag) VALUES ('CSM{PGApJJXcgj}');
 
--- Create a CTF user that matches your repository files:
--- username is in: www/omaga/watdis.txt  -> dausmini
--- password is in: ftp/alahsiaboi.txt   -> passnyadausmini
-CREATE USER IF NOT EXISTS 'dausmini'@'%' IDENTIFIED BY 'passnyadausmini';
-GRANT SELECT ON ctf.* TO 'dausmini'@'%';
-FLUSH PRIVILEGES;
+INSERT INTO flag (flag) VALUES ('CSM{CIAqzhxRqc}');
