86299766aeaa2a78bc834f35ed8b5e9d29bd858d
VeironVaya <veironvayayarief41@gmail.com>
2025-08-27 10:44:18 +0700

    v8
    bind user to database

--- mysql/init.sql
+++ mysql/init.sql
@@ -4,7 +4,15 @@ USE ctfdb;
 
 CREATE TABLE IF NOT EXISTS flag (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    flag VARCHAR(255) NOT NULL
+    flag VARCHAR(255)
 );
 
 INSERT INTO flag (flag) VALUES ('CSM{CIAqzhxRqc}');
+
+-- create user
+CREATE USER 'dausmini'@'%' IDENTIFIED BY 'passnyadausmini';
+
+-- grant them access only to ctfdb
+GRANT ALL PRIVILEGES ON ctfdb.* TO 'dausmini'@'%';
+
+FLUSH PRIVILEGES;
